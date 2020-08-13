8742eec9e7a1379a715c20676ad2aa8fbab76c11
Melstolit <melster19@hotmail.com>
2020-08-13 14:35:14 -0400

    Imported some project to vs2019

--- Chapter01/01_cuda_introduction/01_hello_world/hello_world.cu
+++ Chapter01/01_cuda_introduction/01_hello_world/hello_world.cu
@@ -8,8 +8,8 @@ __global__ void print_from_gpu(void) {
 
 int main(void) { 
 	printf("Hello World from host!\n"); 
-	print_from_gpu<<<1,1>>>();
-	cudaDeviceSynchronize();
+	print_from_gpu<<<2,2>>>();
+	//cudaDeviceSynchronize();
 return 0; 
 }
 
